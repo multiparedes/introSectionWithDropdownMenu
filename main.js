c8ce681735ff7c234f7a3564da62510ba55d4217
Martí Paredes <martiparedes19@gmail.com>
2022-08-19 16:02:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const hambMenu = document.querySelector(".hamburger-link");
+const menuIcon = document.querySelector(".hamburger");
+
+const navMenu = document.querySelector(".navigation-container");
+const navMenuBg = document.querySelector(".bg");
+
+hambMenu.addEventListener("click", () => {
+
+    var menuStat = navMenu.getAttribute("aria-selected");
+
+    if(menuStat == "false") {
+        navMenu.setAttribute("aria-selected", "true")
+        navMenuBg.setAttribute("aria-active", "true")
+
+        menuIcon.style.backgroundImage = "url('images/icon-close-menu.svg')"
+    } else {
+        navMenuBg.setAttribute("aria-active", "false")
+        navMenu.setAttribute("aria-selected", "false")
+
+        menuIcon.style.backgroundImage = "url('images/icon-menu.svg')"
+
+    }
+})
+
+const dropdowns = document.querySelectorAll(".dropdown")
+
+for (let index = 0; index < dropdowns.length; index++) {
+    dropdowns[index].addEventListener("click", () => {
+
+        var dropdownStatus = dropdowns[index].nextElementSibling.getAttribute("aria-active");
+        console.log(dropdownStatus)
+
+        if(dropdownStatus == "false") {
+            dropdowns[index].nextElementSibling.setAttribute("aria-active", "true");
+            dropdowns[index].getElementsByTagName("span")[0].style.backgroundImage = "url('images/icon-arrow-up.svg')"
+        } else {
+            dropdowns[index].nextElementSibling.setAttribute("aria-active", "false")
+            dropdowns[index].getElementsByTagName("span")[0].style.backgroundImage = "url('images/icon-arrow-down.svg')"
+
+        }
+    })    
+}
