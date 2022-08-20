0449ebaa31e6b955943883c5e30bcf552302ebef
Martí Paredes <martiparedes19@gmail.com>
2022-08-20 11:50:16 +0200

    V1.2 - FIXED SOME MINOR ERRORS AND MODIFIED THE NAV MENU

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const hambMenu = document.querySelector(".hamburger-link");
 const menuIcon = document.querySelector(".hamburger");
+const navContainer = document.querySelector(".mobile-nav");
 
 const navMenu = document.querySelector(".navigation-container");
 
@@ -8,14 +9,13 @@ hambMenu.addEventListener("click", () => {
     var menuStat = navMenu.getAttribute("data-shown");
 
     if(menuStat == "false") {
-        navMenu.setAttribute("data-shown", "true")
-
-        menuIcon.style.backgroundImage = "url('images/icon-close-menu.svg')"
+        navMenu.setAttribute("data-shown", "true");
+        navContainer.style.position = "fixed";
+        menuIcon.style.backgroundImage = "url('images/icon-close-menu.svg')";
     } else {
-        navMenu.setAttribute("data-shown", "false")
-
-        menuIcon.style.backgroundImage = "url('images/icon-menu.svg')"
-
+        navMenu.setAttribute("data-shown", "false");
+        navContainer.style.position = "absolute";
+        menuIcon.style.backgroundImage = "url('images/icon-menu.svg')";
     }
 })
 
@@ -25,15 +25,13 @@ for (let index = 0; index < dropdowns.length; index++) {
     dropdowns[index].addEventListener("click", () => {
 
         var dropdownStatus = dropdowns[index].nextElementSibling.getAttribute("data-shown");
-        console.log(dropdownStatus)
 
         if(dropdownStatus == "false") {
             dropdowns[index].nextElementSibling.setAttribute("data-shown", "true");
-            dropdowns[index].getElementsByTagName("span")[0].style.backgroundImage = "url('images/icon-arrow-up.svg')"
+            dropdowns[index].getElementsByTagName("span")[0].style.backgroundImage = "url('images/icon-arrow-up.svg')";
         } else {
-            dropdowns[index].nextElementSibling.setAttribute("data-shown", "false")
-            dropdowns[index].getElementsByTagName("span")[0].style.backgroundImage = "url('images/icon-arrow-down.svg')"
-
+            dropdowns[index].nextElementSibling.setAttribute("data-shown", "false");
+            dropdowns[index].getElementsByTagName("span")[0].style.backgroundImage = "url('images/icon-arrow-down.svg')";
         }
     })    
 }
