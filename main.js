b98d5d23183bd8761f33453354bf8df08e0fe71c
Martí Paredes <martiparedes19@gmail.com>
2022-08-20 11:49:30 +0200

    V1.2 - FIXED SOME MINOR ERRORS AND MODIFIED THE NAV MENU

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const navMenu = document.querySelector(".navigation-container");
 
 hambMenu.addEventListener("click", () => {
 
-    var menuStat = navMenu.getAttribute("aria-selected");
+    var menuStat = navMenu.getAttribute("data-shown");
 
     if(menuStat == "false") {
-        navMenu.setAttribute("aria-selected", "true")
+        navMenu.setAttribute("data-shown", "true")
 
         menuIcon.style.backgroundImage = "url('images/icon-close-menu.svg')"
     } else {
-        navMenu.setAttribute("aria-selected", "false")
+        navMenu.setAttribute("data-shown", "false")
 
         menuIcon.style.backgroundImage = "url('images/icon-menu.svg')"
 
@@ -24,14 +24,14 @@ const dropdowns = document.querySelectorAll(".dropdown")
 for (let index = 0; index < dropdowns.length; index++) {
     dropdowns[index].addEventListener("click", () => {
 
-        var dropdownStatus = dropdowns[index].nextElementSibling.getAttribute("aria-active");
+        var dropdownStatus = dropdowns[index].nextElementSibling.getAttribute("data-shown");
         console.log(dropdownStatus)
 
         if(dropdownStatus == "false") {
-            dropdowns[index].nextElementSibling.setAttribute("aria-active", "true");
+            dropdowns[index].nextElementSibling.setAttribute("data-shown", "true");
             dropdowns[index].getElementsByTagName("span")[0].style.backgroundImage = "url('images/icon-arrow-up.svg')"
         } else {
-            dropdowns[index].nextElementSibling.setAttribute("aria-active", "false")
+            dropdowns[index].nextElementSibling.setAttribute("data-shown", "false")
             dropdowns[index].getElementsByTagName("span")[0].style.backgroundImage = "url('images/icon-arrow-down.svg')"
 
         }
